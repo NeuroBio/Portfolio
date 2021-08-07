9dacfd55a4fd74c92d4422a92bca253e0543e1fb
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-08-07 09:42:10 -0400

    populate experience via d3

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,4 +1,25 @@
-const navLinks = ['Portfolio', 'About', 'Education', 'Connect'];
+const staticText = {
+    brand: `Software engineer looking to work with a collaborative team in data
+    analysis and software development.  Earned PhD from Vanderbilt University
+    in computational biology with five publications.  Took a course in R, and
+    self-taught in C#, Angular, typescript, HTML, and CSS.  Earned a
+    Certificate in Data Analysis from University of Miami, learning Excel,
+    VBA, Python, and SQL.  Developed three live websites and currently acting
+    as a consultant for tech start-up TrustiPhi LLC to design and implement
+    the frontend for their flagship application. Engages in high level
+    discussions with business owners and programmers alike to propose solutions
+    to challenging big picture problems.  Enjoys automating workflow to make
+    tasks faster, easier, and repeatable.  Strives to write readable code and
+    minimize code debt.  Asks for and offers help readily, so that no one is
+    reinventing the wheel.  Well positioned to provide the creativity of an
+    artist and logical approach of a scientist to application development.`,
+    about: `I am scientist redesigning myself into a software engineer.
+    I live for overcoming complex challenges and aim to make 
+    applications and data stories that are efficient, fast, and
+    aesthetically pleasing.`
+}
+
+const navLinks = ['Portfolio', 'About', 'Experience', 'Connect'];
 
 const projects = [
     {
@@ -130,23 +151,109 @@ const projectPopups = [
     },
 ];
 
-const staticText = {
-    brand: `Software engineer looking to work with a collaborative team in data
-    analysis and software development.  Earned PhD from Vanderbilt University
-    in computational biology with five publications.  Took a course in R, and
-    self-taught in C#, Angular, typescript, HTML, and CSS.  Earned a
-    Certificate in Data Analysis from University of Miami, learning Excel,
-    VBA, Python, and SQL.  Developed three live websites and currently acting
-    as a consultant for tech start-up TrustiPhi LLC to design and implement
-    the frontend for their flagship application. Engages in high level
-    discussions with business owners and programmers alike to propose solutions
-    to challenging big picture problems.  Enjoys automating workflow to make
-    tasks faster, easier, and repeatable.  Strives to write readable code and
-    minimize code debt.  Asks for and offers help readily, so that no one is
-    reinventing the wheel.  Well positioned to provide the creativity of an
-    artist and logical approach of a scientist to application development.`,
-    about: ` I am scientist redesigning myself into a software engineer.
-    I live for overcoming complex challenges and aim to make 
-    applications and data stories that are efficient, fast, and
-    aesthetically pleasing.`
-}
+const credentials = [
+    {
+        where: 'TrustiPhi LLC',
+        when: 'Aug 2020 - Present',
+        what: 'Programming Consultant',
+        descInnerHTML: `Using an iterative, mockup process, completed an extensive
+        redesign of TrustiPhi’s flagship angular app to provide a consistent,
+        intuitive user-experience.  Responsible for developing new features of
+        the redesigned application including resolvers and unit tests.  Extended
+        and improved base functionality as well as implementing changes to meet
+        web accessibility standards.  As part of this project, wrote over 50
+        JIRA tasks.`
+    },
+    {
+        where: 'University of Miami',
+        when: 'May 2021 - October 2021',
+        what: 'Certificate in Data Analytics',
+        descInnerHTML: ` Learned Excel, VBA, Python, PostgreSQL, and techniques
+        for big data analytics during a 24-week course. See
+        <a href="https://bootcamp.miami.edu/data/">course description.</a>`
+    },
+    {
+        where: 'Vanderbilt University',
+        when: 'May 2013 - Oct 2019',
+        what: 'Ph.D. in Computational Biology',
+        descInnerHTML: ` <ol><li>Examined how birdsong evolves using a
+        simulation model I developed in R and C#.</li> <li>Compiled published
+        data and performed meta data analysis to understand the relationship
+        between birdsong complexity and reproductive success.</li></ol>
+        <i>Thesis Advisors:</i> Dr. Donna Webb (2014-2017, deceased) & Dr.
+        Nicole Creanza (2017-2019)`,
+        publications: [
+            {
+                citationInnerHTML: `<b>C. Robinson</b>, N. Creanza. (2021). Modeling the evolutionary
+                interactions of sexual selection and learning strategies on the duration
+                of song-learning in birds. <i>PLoS Computational Biology</i>. (under review).`,
+                github: 'https://github.com/CreanzaLab/SongEvolutionModel',
+            },
+            {
+                citationInnerHTML: ` R. Gordon, A. Ravignani, J. Hyland Bruno, <b>C. Robinson</b>, A. Scartozzi,
+                R. Embalabala, M. Niarchou, 23andMe Research Team, N. Cox, N. Creanza.
+                Linking the genomic signatures of human beat synchronization and learned
+                song in birds. <i>Phil Trans B</i>. (in press)`,
+            },
+            {
+                citationInnerHTML: `<b>C. Robinson</b>, K. Snyder, N. Creanza. (2019).
+                Correlated evolution between repertoire size and song plasticity predicts that
+                sexual selection on song promotes open-ended learning. <i>eLife</i>. 8: e44454.`,
+                github: 'https://github.com/CreanzaLab/SongLearningEvolution',
+                link: 'https://elifesciences.org/articles/44454',
+                press: [
+                    {
+                        citationInnerHTML: ` PRESS: <i>The Conversation</i>, September 3, 2019 - Complex birdsongs help biologists piece together the evolution of lifelong learning`,
+                        link: 'https://theconversation.com/complex-birdsongs-help-biologists-piece-together-the-evolution-of-lifelong-learning-122009'
+                    },
+                    {
+                        citationInnerHTML: `PRESS: <i>Research News @ Vanderbilt</i>, High standards of female songbirds could be driving their mates to evolve`,
+                        link: 'https://news.vanderbilt.edu/2019/09/04/high-standards-of-female-songbirds-could-be-driving-their-mates-to-evolve/'
+                    }
+                ]
+
+            },
+            {
+                citationInnerHTML: ` <b>C. Robinson</b>, N. Creanza. (2019). Species‐level repertoire size predicts
+                a correlation between individual song elaboration and reproductive success.
+                <i>Ecology and Evolution</i>. 9(14): 8362-8377.`,
+                github: 'https://github.com/CreanzaLab/RepertoireSizeReproductiveSuccess',
+                link: 'https://onlinelibrary.wiley.com/doi/full/10.1002/ece3.5418',
+            },
+            {
+                citationInnerHTML: `<b>C. Robinson</b>, M. Patel, D. Webb. (2016). Super resolution microscopy is poised to
+                reveal new insights into the formation and maturation of dendritic spines. <i>F1000Research</i>.
+                5(F100 Faculty Rev), 1468.`,
+                link: 'https://f1000research.com/articles/5-1468/v1'
+            },
+            {
+                citationInnerHTML: `J. Evans, <b>C. Robinson</b>, M. Shi, D. Webb. (2015).
+                The guanine nucleotide exchange factor (GEF) asef2 promotes dendritic spine
+                formation via rac activation and spinophilin-dependent targeting.
+                <i>Journal of Biological Chemistry</i>. 290, 10295-10398.`,
+                link: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4400342/',
+            }
+        ]
+    },
+    {
+        where: 'Ohio Northern University',
+        when: 'Aug 2009 - May 2013',
+        what: 'B.S. in Molecular Biology and Behavioral Neuroscience',
+        descInnerHTML: `Research Assistant 2010-2013<br>
+        <i>Research Advisor:</i> Dr. Phillip Zoladz`,
+        publications: [
+            {
+                citationInnerHTML: `P. Zoladz, H. Burke, <b>C. Robinson</b>, S. Woelke, B. Wentz, J. Pisansky,
+                J. McKay, K. Dexter, J. Talbot. (2014). Blunted Corticosterone Response to
+                Acute Predator Stress Results in Long-Term Spatial Memory Impairment. <i>SOJ Psychology</i>. 1(1): 7.`,
+                link: 'https://symbiosisonlinepublishing.com/psychology/psychology02.php',
+            },
+            {
+                citationInnerHTML: `H. Burke, <b>C. Robinson</b>, B. Wentz, J. McKay, K. Dexter., J. Pisansky, J. Talbot, P. Zoladz.
+                (2013). Sex-specific impairment of spatial memory in rats following a reminder of predator stress.
+                <i>Stress</i>. 16(4): 469-76.`,
+                link: 'https://www.tandfonline.com/doi/abs/10.3109/10253890.2013.791276?journalCode=ists20',
+            }
+        ]
+    }
+];
