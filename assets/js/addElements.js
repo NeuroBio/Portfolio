f19195dc94f0b8a4e8a1c31895b3753fc92603c2
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-08-07 09:42:10 -0400

    populate experience via d3

--- assets/js/addElements.js
+++ assets/js/addElements.js
@@ -6,6 +6,8 @@ navLinks.forEach(link => {
         .append('a').attr('class', 'nav-link').attr('href', `#${link.toLowerCase()}`).text(link)
 });
 
+
+
 // Add Projects
 const projectsSection  = d3.select('#projectsSection');
 
@@ -38,6 +40,8 @@ projects.forEach((projectType, i) => {
     }
 });
 
+
+
 // Add Icons
 const languageIcons = d3.select('#icon-row');
 
@@ -53,6 +57,7 @@ icons.forEach(icon => {
             .append('i').attr('class', `fab fa-3x fa-brands ${icon.link}`);
     }
 });
+
 
 
 // Add Project Popups
@@ -94,6 +99,113 @@ projectPopups.forEach(project => {
     description.append('br');
 });
 
+
+
 // Add Credentials
+credentials.forEach((cred, i) => {
+    // set up credential header
+    const container = d3.select('#credentials').append('div').attr('class', 'credential');
+    container
+        .append('h3').attr('class', 'location').text(`${cred.where} | `)
+        .append('span').attr('class', 'credential-date text-muted').text(cred.when);
+    container.append('p').attr('class', 'lead').text(cred.what);
+
+    // set up credential body
+    const desc = container.append('p').attr('class', 'credential-description');
+    desc.html(cred.descInnerHTML);
+
+    // add publications
+    if (cred.publications) {
+        desc.append('br');
+        desc.append('br');
+        desc.append('h4').html('<i>Publications:</i>')
+        const pubList = desc.append('ol');
+        cred.publications.forEach(pub => {
+            const pubDisplay = pubList.append('li').html(pub.citationInnerHTML);
+            // add links to github and article
+            if (pub.github) {
+                pubDisplay
+                    .append('a').attr('class', 'btn-pubs text-center rounded-circle')
+                        .attr('aria-label', 'github-link').attr('href', pub.github)
+                    .append('i').attr('class', 'fab fa-github');
+            }
+            if (pub.link) {
+                pubDisplay
+                .append('a').attr('class', 'btn-pubs text-center rounded-circle')
+                    .attr('aria-label', 'Link-to-article').attr('href', pub.link)
+                .append('i').attr('class', 'bi bi-journal-richtext');
+            }
+            // add press releases
+            if (pub.press) {
+                const pressDisplay = pubDisplay.append('ul');
+                pub.press.forEach(press => {
+                    pressDisplay
+                        .append('li').html(press.citationInnerHTML)
+                        .append('a').attr('class', 'btn-pubs text-center rounded-circle')
+                            .attr('aria-label', 'Link-to-article').attr('href', press.link)
+                        .append('i').attr('class', 'bi bi-newspaper');
+                });
+            }
+        });
+    }
+    if (i < credentials.length - 1) {
+        container.append('hr').attr('class', 'credential-divider');
+    }
+});
 
 
+{/* 
+       
+
+                <ul>
+                    <li>
+                        PRESS: <i>The Conversation</i>, September 3, 2019 - Complex birdsongs help biologists piece together the evolution of lifelong learning
+                        <a class="btn-pubs text-center rounded-circle" aria-label="Link to article"
+                            href="https://theconversation.com/complex-birdsongs-help-biologists-piece-together-the-evolution-of-lifelong-learning-122009">
+                            <i class="bi bi-newspaper"></i>
+                        </a>
+                    </li>
+                    <li>
+                        PRESS: <i>Research News @ Vanderbilt</i>, High standards of female songbirds could be driving their mates to evolve
+                        <a class="btn-pubs text-center rounded-circle" aria-label="Link to article"
+                            href="https://news.vanderbilt.edu/2019/09/04/high-standards-of-female-songbirds-could-be-driving-their-mates-to-evolve/">
+                            <i class="bi bi-newspaper"></i>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <b>C. Robinson</b>, N. Creanza. (2019). Species‐level repertoire size predicts
+                a correlation between individual song elaboration and reproductive success.
+                <i>Ecology and Evolution</i>. 9(14): 8362-8377.
+                <a class="btn-pubs text-center rounded-circle" aria-label="Link to article"
+                    href="https://onlinelibrary.wiley.com/doi/full/10.1002/ece3.5418">
+                    <i class="bi bi-journal-richtext"></i>
+                </a>
+                <a class="btn-pubs text-center rounded-circle" aria-label="github-link"
+                    href="https://github.com/CreanzaLab/RepertoireSizeReproductiveSuccess">
+                        <i class="fab fa-github"></i>
+                </a>
+            </li>
+            <li>
+                <b>C. Robinson</b>, M. Patel, D. Webb. (2016). Super resolution microscopy is poised to
+                reveal new insights into the formation and maturation of dendritic spines. <i>F1000Research</i>.
+                5(F100 Faculty Rev), 1468.
+                <a class="btn-pubs text-center rounded-circle" aria-label="Link to article"
+                    href="https://f1000research.com/articles/5-1468/v1">
+                    <i class="bi bi-journal-richtext"></i>
+                </a>
+            </li>
+            <li>
+                J. Evans, <b>C. Robinson</b>, M. Shi, D. Webb. (2015).
+                The guanine nucleotide exchange factor (GEF) asef2 promotes dendritic spine
+                formation via rac activation and spinophilin-dependent targeting.
+                <i>Journal of Biological Chemistry</i>. 290, 10295-10398.
+                <a class="btn-pubs text-center rounded-circle" aria-label="Link to article"
+                    href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4400342/">
+                    <i class="bi bi-journal-richtext"></i>
+                </a>
+            </li>
+        </ol>
+    </p>
+</div> */}
--- assets/js/addText.js
+++ assets/js/addText.js
@@ -1,6 +1,6 @@
 // Stored Text
 d3.select('#brand-statement').text(staticText.brand);
-d3.select('#about').text(staticText.about);
+d3.select('#about-me').text(staticText.about);
 
 // Copyright info - update as year passes
 const year = new Date().getFullYear()
