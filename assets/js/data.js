20f4537bc2d2408b3bcce287b2c29ad16e105128
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-08-07 07:22:22 -0400

    automate projects and links

---
+++ assets/js/data.js
@@ -0,0 +1,33 @@
+const navLinks = ['Portfolio', 'About', 'Education', 'Connect'];
+
+const projects = [
+    {
+        type: 'Javascript',
+        showcase: [
+            { title: 'Bacterial Biodiversity', number: 4 },
+            { title: 'VtM Maps', number: 6 }
+        ]
+    },
+    {
+        type: 'Angular',
+        showcase: [
+            { title: 'Card Database', number: 3 },
+            { title: 'Arecace', number: 1 },
+            { title: 'Creanza Lab Website', number: 2 }
+        ]
+    },
+    {
+        type: 'C# Blazor',
+        showcase: [
+            { title: 'Song Evolution Model', number: 5 }
+        ]
+    },
+];
+
+const icons = [
+    { type: 'img', link: 'assets/img/logos/r.svg' },
+    { type: 'img', link: 'assets/img/logos/c-sharp.svg' },
+    { type: 'i', link: 'fa-python' },
+    { type: 'i', link: 'fa-angular' },
+    { type: 'img', link: 'assets/img/logos/postgres.svg' },
+];
