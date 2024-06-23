076d36fe10807994c26cc12cb58f59530bdcd34e
kArA <kArARedwing@gmail.com>
2024-06-23 12:13:42 -0400

    fix sizing issues

--- V2/assets/scripts/addText.js
+++ V2/assets/scripts/addText.js
@@ -3,10 +3,10 @@ d3.select('#teaser').text(portfolioData.teaser);
 d3.select('#brand-statement').text(portfolioData.brandStatement);
 // d3.select('#about-me').text(staticText.about);
 
-// // Copyright info - update as year passes
-// const year = new Date().getFullYear()
-// let copyright = 'Copyright © Cristina Robinson 2021'
-// if (year !== 2021) {
-//     copyright += `-${year}`
-// }
-// d3.select('#copyright').text(copyright);
+// Copyright info - update as year passes
+const year = new Date().getFullYear()
+let copyright = 'Copyright © Cristina Robinson 2021'
+if (year !== 2021) {
+    copyright += `-${year}`
+}
+d3.select('#copyright-text').text(copyright);
