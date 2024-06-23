9f9c2fbb614824051ffe936124fec17d99675c5d
kArA <kArARedwing@gmail.com>
2024-06-23 14:33:53 -0400

    start making "reusable components"

---
+++ V2/assets/scripts/addFooter.js
@@ -0,0 +1,41 @@
+const footer = d3.select('footer');
+
+// add socials
+const socials = footer.append('section')
+	.attr('id', 'connect')
+	.text('Connect with me:');
+const socialsList = socials.append('ul')
+	.attr('class', 'flatten');
+
+const linkedIn = socialsList.append('li').append('a')
+	.attr('id', 'linkedin')
+	.attr('href', 'https://www.linkedin.com/in/cristina-robinson-88938052/');
+linkedIn.append('i')
+	.attr('class', 'fab fa-linkedin-in');
+
+
+const gitHub = socialsList.append('li').append('a')
+	.attr('id', 'github')
+	.attr('href', 'https://github.com/NeuroBio');
+gitHub.append('i')
+	.attr('class', 'fab fa-github');
+
+const email = socialsList.append('li').append('a')
+	.attr('id', 'email')
+	.attr('href', 'mailto: cristinamrobinson@gmail.com');
+email.append('i')
+	.attr('class', 'fas fa-envelope');
+
+// add copyright
+const copyRight = footer.append('section')
+	.attr('id', 'copyright')
+	.append('small')
+	.attr('id', 'copyright-text')
+
+// update as year passes
+const year = new Date().getFullYear()
+let copyright = 'Copyright © Cristina Robinson 2021'
+if (year !== 2021) {
+    copyright += `-${year}`
+}
+copyRight.text(copyright);
--- V2/assets/scripts/addText.js
+++ V2/assets/scripts/addText.js
@@ -2,11 +2,3 @@ const portfolioData = new PortfolioData();
 d3.select('#teaser').text(portfolioData.teaser);
 d3.select('#brand-statement').text(portfolioData.brandStatement);
 // d3.select('#about-me').text(staticText.about);
-
-// Copyright info - update as year passes
-const year = new Date().getFullYear()
-let copyright = 'Copyright © Cristina Robinson 2021'
-if (year !== 2021) {
-    copyright += `-${year}`
-}
-d3.select('#copyright-text').text(copyright);
