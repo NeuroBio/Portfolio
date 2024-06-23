7a4a85232110be36260f8cc7252a0eefb400d903
kArA <kArARedwing@gmail.com>
2024-06-23 11:25:37 -0400

    start a V2 from scratch

---
+++ V2/assets/scripts/PortfolioData.js
@@ -0,0 +1,18 @@
+
+
+class PortfolioData {
+	brandStatement = `Driven by challenge and curiosity.
+		I combine an artist's creativity with a scientist's logical approach to solve software engineering problems.
+		Any complex system interests me, from neuronal development to bird song evolution to warehouse inventory management.
+		I’m passionate about reducing code debt, improving testing methods, and learning better architectural design.
+		Though highly independent with my own work, I’m a team player.
+		In discussion, I ask questions and suggest alternative approaches.
+		Reinventing the wheel wastes time, so I readily teach newer devs and talk through code problems.
+		With my academic background, I’m experienced in presenting, data analysis, and formal writing.
+		My illustration and creative writing hobbies have been surprisingly handy at work too.
+		Every job or lab I’ve worked in has recognized my potential and been sad to see me go.
+		Not just for my work ethic, but also for my (apparently) unforgettable laugh and oddball sense of humor.`;
+	teaser = `An-ex scientist, I refactored myself into a software engineer.
+		I live for overcoming complex challenges.  My goal is to make 
+		applications that are maintainable, performant, and aesthetically pleasing.`
+}
---
+++ V2/assets/scripts/addText.js
@@ -0,0 +1,12 @@
+const portfolioData = new PortfolioData();
+d3.select('#teaser').text(portfolioData.teaser);
+d3.select('#brand-statement').text(portfolioData.brandStatement);
+// d3.select('#about-me').text(staticText.about);
+
+// // Copyright info - update as year passes
+// const year = new Date().getFullYear()
+// let copyright = 'Copyright © Cristina Robinson 2021'
+// if (year !== 2021) {
+//     copyright += `-${year}`
+// }
+// d3.select('#copyright').text(copyright);
