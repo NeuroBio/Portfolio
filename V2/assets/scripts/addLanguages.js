3a8ed016535094fca3d820f342c40070543e0978
kArA <kArARedwing@gmail.com>
2024-06-24 18:45:14 -0400

    display language names

--- V2/assets/scripts/addLanguages.js
+++ V2/assets/scripts/addLanguages.js
@@ -1,13 +1,26 @@
-const mainLanguages = d3.select('#main-language-card-container');
+const mainLanguages = PortfolioData.mainLanguages;
+const mainLanguagesElement = d3.select('#main-language-card-container');
+const mainToolName = d3.select('#main-tool-name')
 
-PortfolioData.mainLanguages.forEach((language) => {
-	console.log(language)
-	const iconContainer = mainLanguages.append('article')
-		.attr('class', 'language-card');
+
+function showToolName (id) {
+	const language = mainLanguages.find(x => x.name === id);
+	mainToolName.text(language.description);
+}
+
+function hideToolName () {
+	mainToolName.text('')
+}
+
+mainLanguages.forEach((language) => {
+	const iconContainer = mainLanguagesElement.append('article')
+		.attr('class', 'language-card')
+		.attr('onmouseover', `showToolName('${language.name}')`)
+		.attr('onmouseout', `hideToolName()`);
+	
 	const icon = language.usesLocalImage
 		? iconContainer.append('img')
 		: iconContainer.append('i');
-
 	icon.attr('id', language.name)
 		.attr('src', language.src)
 		.attr('class', language.class)
