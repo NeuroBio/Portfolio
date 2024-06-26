e2640a32d364a07d0fe54dd3461ac8d4ade64cec
kArA <kArARedwing@gmail.com>
2024-06-26 19:38:14 -0400

    restructure data and prep for projects

--- V2/assets/scripts/PortfolioData.js
+++ V2/assets/scripts/PortfolioData.js
@@ -1,3 +1,27 @@
+const LanguageGroupKey = Object.freeze({
+	MAIN: 'main',
+	OTHER: 'other',
+	MISC: 'misc',
+});
+
+const LanguageKey = Object.freeze({
+	NODE: 'Node',
+	NPM: 'npm',
+	GIT: 'git',
+	JIRA: 'Jira',
+	JASMINE: 'Jasmine',
+	PYTHON: 'python',
+	R: 'R',
+	C_SHARP: 'C#',
+	ANGULAR: 'angular',
+	REACT: 'react',
+	GITHUB: 'github',
+	FIREBASE: 'firebase',
+	JEST: 'jest',
+	CONFLUENCE: 'confluence',
+	MARKDOWN: 'markdown',
+	BITBUCKET: 'bitbucket',
+});
 
 class Language {
 	constructor(shortName, longName, src) {
@@ -20,19 +44,47 @@ class Book {
 		this.link = link;
 	}
 }
-
 class Passion {
 	constructor(title, description) {
 		this.title = title;
 		this.description = description;
 	}
 }
+class Link {
+	constructor(title, src) {
+		this.title = title;
+		this.src = src;
+	}
+}
+class Project {
+	constructor(title, previewImage, description, languages, links) {
+		this.title = title;
+		this.previewImage = previewImage;
+		this.description = description;
+		this.languages = languages;
+		this.links = links;
+	}
+}
 
-const LanguageKeys = {
-	MAIN: 'main',
-	OTHER: 'other',
-	MISC: 'misc',
-};
+const AllLanguages = Object.freeze({
+	[LanguageKey.NODE]: new Language('Node', 'Node.js', 'fab fa-node-js'),
+	[LanguageKey.NPM]: new Language('npm', 'Node Package Manager', 'fab fa-npm'),
+	[LanguageKey.GIT]: new Language('git', 'Git', 'fab fa-git'),
+	[LanguageKey.JIRA]: new Language('Jira', 'Jira', 'fab fa-jira'),
+	[LanguageKey.JASMINE]: new Language('Jasmine', 'Jasmine (Testing Framework)', '../assets/images/logos/jasmine.svg'),	
+	[LanguageKey.PYTHON]: new Language('python', 'Python', 'fab fa-python'),
+	[LanguageKey.R]: new Language('R', 'R', 'fab fa-r-project'),
+	[LanguageKey.C_SHARP]: new Language('C#', 'C#', '../assets/images/logos/c-sharp.svg'),
+	[LanguageKey.ANGULAR]: new Language('angular', 'Angular (Web Framework)', 'fab fa-angular'),
+	[LanguageKey.REACT]: new Language('react', 'React (Web Framework)', 'fab fa-react'),
+	[LanguageKey.GITHUB]: new Language('github', 'GitHub', 'fab fa-github-square'),
+	[LanguageKey.FIREBASE]: new Language('firebase', 'Firebase', '../assets/images/logos/firebase.svg'),
+	[LanguageKey.JEST]: new Language('jest', 'Jest (Testing Framework)', '../assets/images/logos/jest.svg'),
+	[LanguageKey.CONFLUENCE]: new Language('confluence', 'Confluence', 'fab fa-confluence'),
+	[LanguageKey.MARKDOWN]: new Language('markdown', 'Markdown', 'fab fa-markdown'),
+	[LanguageKey.BITBUCKET]: new Language('bitbucket', 'Bitbucket', 'fab fa-bitbucket'),
+});
+
 
 const PortfolioData = {
 	brandStatement: `Driven by challenge and curiosity.
@@ -51,6 +103,26 @@ const PortfolioData = {
 		I live for overcoming complex challenges.  My goal is to make 
 		applications that are maintainable, performant, and aesthetically pleasing.`,
 
+	projects: [
+		new Project(
+			'Arecace (Worldbuilding Project)',
+			'assets/images/projects/project1.png',
+			`A showcase website for my original story and Worldbuilding project.
+			This was one of my first websites.
+			It was where I began to understand the power of modular code...
+			and started to feel the pain of refactoring code without automated tests.
+			`,
+			[
+				AllLanguages[LanguageKey.ANGULAR],
+				AllLanguages[LanguageKey.FIREBASE],
+			],
+			[
+				new Link('Live Project', 'https://arecace.com/home'),
+				new Link('Repo', 'https://github.com/NeuroBio/ArecaceWebsite'),
+			]
+		),
+	],
+	
 	passions: [
 		new Passion(
 			'Test-Driven Development',
@@ -83,27 +155,27 @@ const PortfolioData = {
 	],
 
 	languages: {
-		[LanguageKeys.MAIN]: {
-			Node: new Language('Node', 'Node.js', 'fab fa-node-js'),
-			npm: new Language('npm', 'Node Package Manager', 'fab fa-npm'),
-			git: new Language('git', 'Git', 'fab fa-git'),
-			Jira: new Language('Jira', 'Jira', 'fab fa-jira'),
-			Jasmine: new Language('Jasmine', 'Jasmine (Testing Framework)', '../assets/images/logos/jasmine.svg'),		
+		[LanguageGroupKey.MAIN]: {
+			[LanguageKey.NODE]: AllLanguages[LanguageKey.NODE],
+			[LanguageKey.NPM]: AllLanguages[LanguageKey.NPM],
+			[LanguageKey.GIT]: AllLanguages[LanguageKey.GIT],
+			[LanguageKey.JIRA]: AllLanguages[LanguageKey.JIRA],
+			[LanguageKey.JASMINE]: AllLanguages[LanguageKey.JASMINE],		
 		},
-		[LanguageKeys.OTHER]: {
-			python: new Language('python', 'Python', 'fab fa-python'),
-			R: new Language('R', 'R', 'fab fa-r-project'),
-			'C#': new Language('C#', 'C#', '../assets/images/logos/c-sharp.svg'),
-			angular: new Language('angular', 'Angular (Web Framework)', 'fab fa-angular'),
-			react: new Language('react', 'React (Web Framework)', 'fab fa-react'),
+		[LanguageGroupKey.OTHER]: {
+			[LanguageKey.PYTHON]: AllLanguages[LanguageKey.PYTHON],
+			[LanguageKey.R]: AllLanguages[LanguageKey.R],
+			[LanguageKey.C_SHARP]: AllLanguages[LanguageKey.C_SHARP],
+			[LanguageKey.ANGULAR]: AllLanguages[LanguageKey.ANGULAR],
+			[LanguageKey.REACT]: AllLanguages[LanguageKey.REACT],
 		},
-		[LanguageKeys.MISC]: {
-			github: new Language('github', 'GitHub', 'fab fa-github-square'),
-			firebase: new Language('firebase', 'Firebase', '../assets/images/logos/firebase.svg'),
-			jest: new Language('jest', 'Jest (Testing Framework)', '../assets/images/logos/jest.svg'),
-			confluence: new Language('confluence', 'Confluence', 'fab fa-confluence'),
-			markdown: new Language('markdown', 'Markdown', 'fab fa-markdown'),
-			bitbucket: new Language('bitbucket', 'Bitbucket', 'fab fa-bitbucket'),
+		[LanguageGroupKey.MISC]: {
+			[LanguageKey.GITHUB]: AllLanguages[LanguageKey.GITHUB],
+			[LanguageKey.FIREBASE]: AllLanguages[LanguageKey.FIREBASE],
+			[LanguageKey.JEST]: AllLanguages[LanguageKey.JEST],
+			[LanguageKey.CONFLUENCE]: AllLanguages[LanguageKey.CONFLUENCE],
+			[LanguageKey.MARKDOWN]: AllLanguages[LanguageKey.MARKDOWN],
+			[LanguageKey.BITBUCKET]: AllLanguages[LanguageKey.BITBUCKET],
 		},
 	},
 }
---
+++ V2/assets/scripts/addProjects.js
@@ -0,0 +1,5 @@
+const projectContainer = d3.select('#projects');
+
+PortfolioData.projects.forEach((project) => {
+	
+});
