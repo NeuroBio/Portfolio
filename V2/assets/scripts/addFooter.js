fc627e021e77dd103c30cc32d91b5cc04afa247b
kArA <kArARedwing@gmail.com>
2024-07-03 06:19:49 -0400

    refactoring

--- V2/assets/scripts/addFooter.js
+++ V2/assets/scripts/addFooter.js
@@ -1,41 +1,50 @@
 const footer = d3.select('footer');
 
-// add socials
 const socials = footer.append('section')
 	.attr('id', 'connect')
 	.text('Connect with me:');
 const socialsList = socials.append('ul')
 	.attr('class', 'flatten');
 
+addSocial({
+	socialsList,
+	link: 'https://github.com/NeuroBio',
+	text: 'Link to my Github',
+	iconClass: 'fab fa-github',
+});
+addSocial({
+	socialsList,
+	link: 'https://www.linkedin.com/in/cristina-robinson-88938052/',
+	text: 'Link to my LinkedIn',
+	iconClass: 'fab fa-linkedin',
+});
+addSocial({
+	socialsList,
+	link: 'mailto: cristinamrobinson@gmail.com',
+	text: 'Link to my email',
+	iconClass: 'fas fa-envelope',
+});
 
-const gitHub = socialsList.append('li').append('a')
-	.attr('aria-label', 'Link to my Github')
-	.attr('href', 'https://github.com/NeuroBio');
-gitHub.append('i')
-	.attr('class', 'fab fa-github');
 
-const linkedIn = socialsList.append('li').append('a')
-	.attr('aria-label', 'Link to my LinkedIn')
-	.attr('href', 'https://www.linkedin.com/in/cristina-robinson-88938052/');
-linkedIn.append('i')
-	.attr('class', 'fab fa-linkedin');
-
-const email = socialsList.append('li').append('a')
-	.attr('aria-label', 'Link to my email')
-	.attr('href', 'mailto: cristinamrobinson@gmail.com');
-email.append('i')
-	.attr('class', 'fas fa-envelope');
-
-// add copyright
 const copyRight = footer.append('section')
 	.attr('id', 'copyright')
-	.append('small')
+copyRight.append('small')
 	.attr('id', 'copyright-text')
+	.text(getCopyRightWithCurrentYear());
 
-// update as year passes
-const year = new Date().getFullYear()
-let copyright = 'Copyright © Cristina Robinson 2021'
-if (year !== 2021) {
-    copyright += `-${year}`
+
+function addSocial({ socialsList, link, text, iconClass }) {
+	const social = socialsList.append('li').append('a')
+		.attr('aria-label', text)
+		.attr('href', link);
+	social.append('i').attr('class', iconClass);
 }
-copyRight.text(copyright);
+function getCopyRightWithCurrentYear() {
+	const year = new Date().getFullYear()
+	let copyright = 'Copyright © Cristina Robinson 2021'
+	if (year !== 2021) {
+		copyright += `-${year}`
+	}
+	return copyright;
+}
+
