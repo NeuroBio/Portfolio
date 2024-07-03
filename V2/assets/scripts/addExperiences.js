68dab71d3228efe548e80ffb3f883811a00f24b9
kArA <kArARedwing@gmail.com>
2024-07-03 06:19:49 -0400

    refactoring

--- V2/assets/scripts/addExperiences.js
+++ V2/assets/scripts/addExperiences.js
@@ -11,11 +11,26 @@ PortfolioData.experiences.education.forEach((experience) => buildAccordion(exper
 
 
 function buildAccordion (experience) {
-	const { organization, safeId, dateRange, positions, achievements, publications, startExpanded } = experience;
+	const { safeId, positions, achievements, publications, startExpanded } = experience;
 
-	const isExpandable = (positions.length > 0 || achievements.length > 0 || publications.length > 0);
 	const epoch = experienceContainer.append('article')
 		.attr('class', 'epoch');
+
+	buildHeader(epoch, experience);
+
+	const epochContents = epoch.append('section')
+		.attr('id', `${safeId}-panel`)
+		.attr('class', `accordion-panel ${startExpanded ? 'expanded' : 'collapsed'}`);
+
+	addPositions({ epochContents, positions });
+	addAchievements({ epochContents, achievements });
+	addPublications({ epochContents, publications });
+}
+
+function buildHeader (epoch, experience) {
+	const { organization, safeId, dateRange, positions, achievements, publications, startExpanded } = experience;
+	const isExpandable = (positions.length > 0 || achievements.length > 0 || publications.length > 0);
+
 	const button = epoch.append('button')
 		.attr('id', safeId)
 		.attr('type', 'button')
@@ -41,28 +56,33 @@ function buildAccordion (experience) {
 	} else {
 		button.append('i').attr('class', `not-expandable accordion-icon fas fa-circle`);
 	}
+}
 
-	const epochContents = epoch.append('section')
-		.attr('id', `${safeId}-panel`)
-		.attr('class', `accordion-panel ${startExpanded ? 'expanded' : 'collapsed'}`);
-
-	if (positions.length > 0) {
-		epochContents.append('h4').text('Positions');
-		const positionList = epochContents.append('ul');
-		positions.forEach((position) => {
-			const positionEntry = positionList.append('li')
-				.attr('class', 'position')
-
-			positionEntry.append('span').text(position.title);
-			const endPeriod = position.endYear === PRESENT
-				? PRESENT
-				: `${position.endMonth}, ${position.endYear}`
-			positionEntry.append('i').text(`(${position.startMonth}, ${position.startYear} - ${endPeriod})`);
-		});
+function addPositions ({ epochContents, positions }) {
+	if (positions.length === 0) {
+		return
 	}
 
-	if (achievements.length > 0) {
-		epochContents.append('h4').text('Achievements');
+	epochContents.append('h4').text('Positions');
+	const positionList = epochContents.append('ul');
+	positions.forEach((position) => {
+		const positionEntry = positionList.append('li')
+			.attr('class', 'position')
+
+		positionEntry.append('span').text(position.title);
+		const endPeriod = position.endYear === PRESENT
+			? PRESENT
+			: `${position.endMonth}, ${position.endYear}`
+		positionEntry.append('i').text(`(${position.startMonth}, ${position.startYear} - ${endPeriod})`);
+	});
+}
+
+function addAchievements ({ epochContents, achievements }) {
+	if (achievements.length === 0) {
+		return
+	}
+
+	epochContents.append('h4').text('Achievements');
 		const achievementsList = epochContents.append('ul');
 		achievements.forEach((achievement) => {
 			const achievementEntry = achievementsList.append('li')
@@ -71,48 +91,58 @@ function buildAccordion (experience) {
 			achievementEntry.append('span').text(achievement.text);
 			achievementEntry.append('i').text(achievement.date);
 		});
-	}
-
-	if (publications.length > 0) {
-		epochContents.append('h4').text('Publications');
-		const publicationList = epochContents.append('ul');
-		publications.forEach((publication) => {
-			const publicationEntry = publicationList.append('li')
-				.attr('class', 'publication')
-			publicationEntry.append('span').html(publication.citation);
-			const publicationLinks = publicationEntry.append('span')
-				.attr('class', 'publicationLinks');
-			const articleLink = publicationLinks.append('a')
-				.attr('aria-label', 'journal article link')
-				.attr('href', publication.articleLink)
-				.attr('class', 'publicationLink');
-			articleLink.append('i').attr('class', 'fas fa-file-invoice')
-			if (publication.githubLink) {
-				const githubLink = publicationLinks.append('a')
-					.attr('aria-label', 'github repo link')
-					.attr('href', publication.githubLink)
-					.attr('class', 'publicationLink');
-				githubLink.append('i').attr('class', 'fab fa-github-square')	
-			}
-
-			if (publication.pressReleases) {
-				const pressReleaseList = publicationList.append('ul')
-					.attr('class', 'press-releases');
-				publication.pressReleases.forEach(release => {
-					const pressReleaseEntry = pressReleaseList.append('li')
-						.attr('class', 'publication');
-					pressReleaseEntry.append('span').html(release.citation);
-					const pressReleaseLink = pressReleaseEntry.append('a')
-						.attr('aria-label', 'Press release link')
-						.attr('href', release.githubLink)
-						.attr('class', 'publicationLink');
-					pressReleaseLink.append('i').attr('class', 'fas fa-newspaper');
-				});
-			}
-		});
-	}
 }
 
+function addPublications ({ epochContents, publications }) {
+	if (publications.length === 0) {
+		return
+	}
+
+	epochContents.append('h4').text('Publications');
+	const publicationList = epochContents.append('ul');
+	publications.forEach((publication) => {
+		const publicationEntry = publicationList.append('li')
+			.attr('class', 'publication')
+		publicationEntry.append('span').html(publication.citation);
+		const publicationLinks = publicationEntry.append('span')
+			.attr('class', 'publicationLinks');
+		const articleLink = publicationLinks.append('a')
+			.attr('aria-label', 'journal article link')
+			.attr('href', publication.articleLink)
+			.attr('class', 'publicationLink');
+		articleLink.append('i').attr('class', 'fas fa-file-invoice')
+		if (publication.githubLink) {
+			const githubLink = publicationLinks.append('a')
+				.attr('aria-label', 'github repo link')
+				.attr('href', publication.githubLink)
+				.attr('class', 'publicationLink');
+			githubLink.append('i').attr('class', 'fab fa-github-square')	
+		}
+
+		addPressReleases({ publicationList, pressReleases: publication.pressReleases });
+	});
+}
+
+function addPressReleases ({ publicationList, pressReleases }) {
+	if (!pressReleases || pressReleases.length === 0) {
+		return
+	}
+
+	const pressReleaseList = publicationList.append('ul')
+		.attr('class', 'press-releases');
+	pressReleases.forEach(release => {
+		const pressReleaseEntry = pressReleaseList.append('li')
+			.attr('class', 'publication');
+		pressReleaseEntry.append('span').html(release.citation);
+		const pressReleaseLink = pressReleaseEntry.append('a')
+			.attr('aria-label', 'Press release link')
+			.attr('href', release.githubLink)
+			.attr('class', 'publicationLink');
+		pressReleaseLink.append('i').attr('class', 'fas fa-newspaper');
+	});
+}
+
+// web element behaviors
 function expandAccordion (organization) {
 	console.log('expanding...', organization)
 	d3.select(`#${organization}`)
--- V2/assets/scripts/addText.js
+++ V2/assets/scripts/addText.js
@@ -1,3 +1,1 @@
 d3.select('#teaser').text(PortfolioData.teaser);
-d3.select('#brand-statement').text(PortfolioData.brandStatement);
-// d3.select('#about-me').text(staticText.about);
